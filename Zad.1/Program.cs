26570636d30a5d51208a4eeeddc6877327d553e7
Beihan <beihan99@mail.bg>
2018-10-29 17:17:57 +0200

    Add project files.

---
+++ Zad.1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Zad._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            ushort number1 = 52130;
+            Console.WriteLine(number1);
+
+            sbyte number2 = -115;
+            Console.WriteLine(number2);
+
+            uint number3 = 4825932;
+            Console.WriteLine(number3);
+
+            byte number4 = 97;
+            Console.WriteLine(number4);
+
+            short number5 = -10000;
+            Console.WriteLine(number5);
+
+            short number6 = 20000;
+            Console.WriteLine(number6);
+
+            byte number7 = 224;
+            Console.WriteLine(number7);
+
+            uint number8 = 970700000;
+            Console.WriteLine(number8);
+
+            sbyte number9 = 112;
+            Console.WriteLine(number9);
+
+            sbyte number10 = -44;
+            Console.WriteLine(number10);
+
+            int number11 = -1000000;
+            Console.WriteLine(number11);
+
+            ushort number12 = 1992;
+
+            long number13 = 123456789123456789;
+            Console.WriteLine(number13);
+
+        }
+    }
+}
