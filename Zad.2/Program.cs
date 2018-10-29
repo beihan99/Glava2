3f7a90a0d5387c40412945592d38d724a81feb32
Beihan <beihan99@mail.bg>
2018-10-29 17:28:56 +0200

    Zad.2

---
+++ Zad.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Zad._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            double number1 = 34.567839023;
+            Console.WriteLine(number1);
+
+            float number2 = 12.345f;
+            Console.WriteLine(number2);
+
+            double number3 = 8923.1234857;
+            Console.WriteLine(number3);
+
+            decimal number4 = 3456.091124875956542151256683467m;
+            Console.WriteLine(number4);
+        }
+    }
+}
